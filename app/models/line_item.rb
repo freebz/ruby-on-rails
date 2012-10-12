ddbead712e8c5f76d8764c83d9930ae9fd3edeac
FreeBz <freebz@hananet.net>
2012-10-12 16:33:31 +0900

    'ajax'

--- app/models/line_item.rb
+++ app/models/line_item.rb
@@ -3,7 +3,6 @@ class LineItem < ActiveRecord::Base
 	belongs_to :cart
 	
 	attr_accessible :cart_id, :product_id, :cart, :product
-	#attr_accessible :cart, :product
 	
 	def total_price
 		product.price * quantity
--- db/migrate/20121012022038_add_quantity_to_line_times.rb
+++
@@ -1,5 +0,0 @@
-class AddQuantityToLineTimes < ActiveRecord::Migration
-  def change
-    add_column :line_items, :quantity, :integer, default: 1
-  end
-end
