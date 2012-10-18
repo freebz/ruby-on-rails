932c47362d1e0ff5b6d4417cc11cfc5413228e8e
FreeBz <freebz@hananet.net>
2012-10-18 11:22:04 +0900

    Internationalization

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -58,7 +58,7 @@ class OrdersController < ApplicationController
 			session[:cart_id] = nil
 			#OrderNotifier.order_received(@order).deliver
 			OrderNotifier.received(@order).deliver
-      format.html { redirect_to(store_url, notice: 'Thank you for your order.') }
+      format.html { redirect_to(store_url, notice: I18n.t('.thanks')) }
       format.json { render json: @order, status: :created, location: @order }
     else
 			@cart = current_cart
